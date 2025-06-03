104623fb0cb94c613e723837519aaffb0c755f7d
harsh-devjs <harsh.devjs@gmail.com>
2025-06-03 18:02:46 +0530

    initial front-end commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-7xl font-black text-purple-600'>hello friend</h1>
+    </div>
+  )
+}
+
+export default App

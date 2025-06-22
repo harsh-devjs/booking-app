c761df932bf88eca1cd3eff8cc6c144fd41b3fed
harsh-devjs <harsh.devjs@gmail.com>
2025-06-22 23:19:53 +0530

    removed isAdmin option from register route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -11,8 +11,7 @@ export const register = async (req, res, next) => {
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: hash,
-            isAdmin: req.body.isAdmin
+            password: hash
         })
 
         await newUser.save()

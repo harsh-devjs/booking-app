e900a45ed662de901381350f9c05e77a1c6eb7b0
harsh-devjs <harsh.devjs@gmail.com>
2025-06-24 15:35:13 +0530

    created room route

---
+++ api/routes/rooms.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import { createRoom, deleteRoom, getAllRooms, getRoom, updateRoom } from "../controllers/room.js";
+
+const router = express.Router()
+
+// CREATE
+router.post('/:hotelId', verifyAdmin, createRoom)
+
+// UPDATE
+router.put('/:id', verifyAdmin, updateRoom)
+
+// DELETE
+router.delete('/:id', verifyAdmin, deleteRoom)
+
+// GET
+router.get('/:id', getRoom)
+
+// GET ALL
+router.get('/', getAllRooms)
+
+export default router

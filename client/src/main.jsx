61a84dab0a4d57dcc375d08f46d9f31a7c30efd1
harsh-devjs <harsh.devjs@gmail.com>
2025-06-30 16:30:33 +0530

    wrap app with search context provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SearchContextProvider } from './context/SearchContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SearchContextProvider>
+      <App />
+    </SearchContextProvider>
   </StrictMode>,
 )

ba7734defeda872862bc854db8d20d57d6357481
harsh-devjs <harsh.devjs@gmail.com>
2025-07-01 20:26:41 +0530

    change state date to dates

--- client/src/pages/Hotels.jsx
+++ client/src/pages/Hotels.jsx
@@ -14,7 +14,7 @@ const Hotels = () => {
 
   const location = useLocation()
   const [destination, setDestination] = useState(location.state.destination)
-  const [date, setDate] = useState(location.state.date)
+  const [dates, setDates] = useState(location.state.dates)
   const [openDate, setOpenDate] = useState(false)
   const [options, setOptions] = useState(location.state.options)
   const [min, setMin] = useState(undefined)
@@ -61,12 +61,12 @@ const Hotels = () => {
               <span
                 onClick={() => setOpenDate(prev => !prev)}
                 className="text-gray-800 p-2.5 bg-white flex items-center cursor-pointer">
-                {`${format(date[0].startDate, 'dd/MM/yyyy')} to ${format(date[0].endDate, 'dd/MM/yyyy')}`}
+                {`${format(dates[0].startDate, 'dd/MM/yyyy')} to ${format(dates[0].endDate, 'dd/MM/yyyy')}`}
               </span>
               {openDate && <DateRange
                 onChange={item => setDate([item.selection])}
                 minDate={new Date()}
-                ranges={date}
+                ranges={dates}
               />}
             </div>
 
